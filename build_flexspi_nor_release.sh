e92d01a5ada3d06d37520162a7e793e9d80f2842
pengunix <hoshimaemi@outlook.com>
2025-02-01 09:32:30 +0800

    init

---
+++ build_flexspi_nor_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export ARMGCC_DIR=/home/naii/rt1064/toolchain/gcc-arm-none-eabi-10.3-2021.10/
+export SDK_DIR=/home/naii/rt1064/sdk
+cd build/
+if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
+if [ -f "Makefile" ];then rm -f Makefile; fi
+if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
+if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
+cmake -DSdkRootDirPath=${SDK_DIR} -DCMAKE_TOOLCHAIN_FILE=${SDK_DIR}"/tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=flexspi_nor_release  ..
+make -j 2>&1 | tee build_log.txt
---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf flexspi_nor_debug flexspi_nor_release 
+cd build/
+rm -rf Makefile cmake_install.cmake CMakeCache.txt CMakeFiles output.map build_log.txt compile_commands.json
